42e607cbffe054cff154469bfb36eeeb601d3355
Jeong Kwangkyun <rhkdrbs06@naver.com>
2021-05-04 23:40:11 +0900

    Initial Commit

---
+++ Country_Quiz WatchKit Extension/InterfaceController.swift
@@ -0,0 +1,31 @@
+//
+//  InterfaceController.swift
+//  Country_Quiz WatchKit Extension
+//
+//  Created by 정광균 on 2021/05/04.
+//  Copyright © 2021 정광균. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+
+
+class InterfaceController: WKInterfaceController {
+
+    override func awake(withContext context: Any?) {
+        super.awake(withContext: context)
+        
+        // Configure interface objects here.
+    }
+    
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+    
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+}
---
+++ Country_Quiz WatchKit Extension/NotificationController.swift
@@ -0,0 +1,38 @@
+//
+//  NotificationController.swift
+//  Country_Quiz WatchKit Extension
+//
+//  Created by 정광균 on 2021/05/04.
+//  Copyright © 2021 정광균. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import UserNotifications
+
+
+class NotificationController: WKUserNotificationInterfaceController {
+
+    override init() {
+        // Initialize variables here.
+        super.init()
+        
+        // Configure interface objects here.
+    }
+
+    override func willActivate() {
+        // This method is called when watch view controller is about to be visible to user
+        super.willActivate()
+    }
+
+    override func didDeactivate() {
+        // This method is called when watch view controller is no longer visible
+        super.didDeactivate()
+    }
+
+    override func didReceive(_ notification: UNNotification) {
+        // This method is called when a notification needs to be presented.
+        // Implement it if you use a dynamic notification interface.
+        // Populate your dynamic notification interface as quickly as possible.
+    }
+}
